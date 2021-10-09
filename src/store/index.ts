6201d74bfa1eaa9dc48f35982ffc1e33d8bc4f2b
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2021-10-09 16:13:03 +0500

    remove consoles

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,14 +8,11 @@ export default createStore({
   },
   mutations: {
     CALCULATE_SUM(state, { optionA, optionB }) {
-      console.log("CALCULATE_SUM is: ", optionA);
-      console.log("CALCULATE_SUM is: ", optionB);
       state.sum = optionA + optionB;
     },
   },
   actions: {
     calculateSum({ commit }, payload) {
-      console.log("paylad is: ", payload);
       commit("CALCULATE_SUM", payload);
     },
   },
